2c820e49a781b39fac02c435f1d8d2d7ed99e101
Petrov Anton <gg4every@gmail.com>
2019-02-06 19:01:17 +0400

    Invert if statement

--- app/src/main/java/website/petrov/noue/utilities/UI.java
+++ app/src/main/java/website/petrov/noue/utilities/UI.java
@@ -17,42 +17,44 @@ public final class UI {
     public static void setFullscreenLayout(@NonNull Context context, boolean darkIcon) {
         final Activity activity = Provider.getActivity(context);
 
-        if (activity != null) {
-            final Window window = activity.getWindow();
-            final View decor = window.getDecorView();
-            int visibility = decor.getSystemUiVisibility();
-
-            final int clear =
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS |
-                            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
-            final int add =
-                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS |
-                            WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS;
-
-            window.setNavigationBarColor(Color.TRANSPARENT);
-            window.setStatusBarColor(Color.TRANSPARENT);
-
-            window.clearFlags(clear);
-            window.addFlags(add);
-
-            if (darkIcon) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    visibility |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-                }
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    visibility |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
-                }
-            } else {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    visibility &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-                }
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    visibility &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
-                }
-            }
-
-            decor.setSystemUiVisibility(visibility);
+        if (activity == null) {
+            return;
         }
+
+        final Window window = activity.getWindow();
+        final View decor = window.getDecorView();
+        int visibility = decor.getSystemUiVisibility();
+
+        final int clear =
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS |
+                        WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+        final int add =
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS |
+                        WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS;
+
+        window.setNavigationBarColor(Color.TRANSPARENT);
+        window.setStatusBarColor(Color.TRANSPARENT);
+
+        window.clearFlags(clear);
+        window.addFlags(add);
+
+        if (darkIcon) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                visibility |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                visibility |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                visibility &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                visibility &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+            }
+        }
+
+        decor.setSystemUiVisibility(visibility);
     }
 
     private UI() {} // hide constructor
