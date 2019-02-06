d2013173dadf8bb8fa293fd0b89cc05913399d4d
Petrov Anton <gg4every@gmail.com>
2019-02-06 18:43:21 +0400

    Remove use of OS-specific methods

--- app/src/main/java/website/petrov/noue/utilities/UI.java
+++ app/src/main/java/website/petrov/noue/utilities/UI.java
@@ -1,6 +1,5 @@
 package website.petrov.noue.utilities;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
@@ -11,36 +10,7 @@ import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-@SuppressWarnings("JavaReflectionMemberAccess")
-@SuppressLint("PrivateApi")
 public final class UI {
-    private static void setTransparentStatusBarFlyme(@NonNull Window window) {
-        try {
-            final WindowManager.LayoutParams lp = window.getAttributes();
-            final Field darkFlag = WindowManager.LayoutParams.class.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
-            darkFlag.setAccessible(true);
-            final Field meizuFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
-            meizuFlags.setAccessible(true);
-            final int value = meizuFlags.getInt(lp) | darkFlag.getInt(null);
-            meizuFlags.setInt(lp, value);
-            window.setAttributes(lp);
-        } catch (Throwable ignored) {
-        }
-    }
-
-    private static void setTransparentStatusBarMIUI(@NonNull Window window) {
-        try {
-            final Class layoutParamsClass = Class.forName("android.view.MiuiWindowManager$LayoutParams");
-            final int tranceFlag = layoutParamsClass.getField("EXTRA_FLAG_STATUS_BAR_TRANSPARENT").getInt(null);
-            final Method extraFlagsField = Window.class.getMethod("setExtraFlags", int.class, int.class);
-            extraFlagsField.invoke(window, tranceFlag, tranceFlag);
-        } catch (Throwable ignored) {
-        }
-    }
-
     /**
      * @param context the context
      */
@@ -80,6 +50,7 @@ public final class UI {
                     visibility &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
                 }
             }
+
             decor.setSystemUiVisibility(visibility);
         }
     }
