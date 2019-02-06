0296800b708ebc7481bba56d29b7ff92836c1905
Petrov Anton <gg4every@gmail.com>
2019-02-06 18:25:58 +0400

    Revert "Fix "Non-primitive fields should not be 'volatile'""
    This reverts commit cbbb55ade120e9b8f951a59e38cb48428021599d.

--- app/src/main/java/website/petrov/noue/repository/data/StorageDatabase.java
+++ app/src/main/java/website/petrov/noue/repository/data/StorageDatabase.java
@@ -22,7 +22,7 @@ import website.petrov.noue.utilities.Converters;
 @TypeConverters({Converters.class})
 public abstract class StorageDatabase extends RoomDatabase {
     @Nullable
-    private static StorageDatabase mInstance;
+    private static volatile StorageDatabase mInstance;
 
     @Contract("_ -> !null")
     static StorageDatabase getDatabase(@NonNull Context context) {
